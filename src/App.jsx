fc113a5c365c9f6a85299eea8a2906acce892a46
Kameli71 <karim.kameli71@gmail.com>
2022-09-26 14:20:49 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Register from './pages/Register';
+// import Product from './pages/Product';
+// import ProductList from './pages/ProductList';
+// import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Home /> */}
+      {/* <ProductList /> */}
+      {/* <Product /> */}
+      <Register />
+    </div>
+  );
+}
+
+export default App;
